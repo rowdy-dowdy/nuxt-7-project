692098f2590f4080fba8958997f8b70eb3114392
Việt Hùng <viet.hung.2898@gmail.com>
2022-09-17 17:34:16 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  typescript: {
+    shim: false
+  },
+  modules: [
+    // "@nuxtjs/axios",
+    // '@nuxtjs/auth-next',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+  // publicRuntimeConfig: {
+  //   axios: {
+  //     baseURL: process.env.API_URL || ' http://localhost:3000/api/',
+  //   },
+  // },
+  // axios: {
+  //   baseURL: process.env.API_URL || ' http://localhost:3000/api/',
+  //   debug: process.env.DEBUG || false,
+  //   proxyHeaders: false,
+  //   credentials: false,
+  // },
+  // auth: {
+  //   // Options
+  //   redirect: {
+  //     login: '/login',
+  //     logout: '/',
+  //     home: '/'
+  //   }
+  // }
 })
